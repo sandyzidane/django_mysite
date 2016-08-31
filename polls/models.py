8cf84685e510ceddcf175e91d5a95681b5c313c9
sandy <sandyhysbz@gmail.com>
2016-08-31 13:58:38 +0800

    完成教程3，学会写view，学会部分template写法

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 import datetime
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils import timezone
 
 # Create your models here.
